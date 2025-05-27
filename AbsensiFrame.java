8940596a2161bf1989576282c2be70e44370a4d0
Regina Caeli Endyarni <143944696+Reginace@users.noreply.github.com>
2025-05-27 23:01:46 +0700

    Update AbsensiFrame.java
    revisi frame absensi

--- AbsensiFrame.java
+++ AbsensiFrame.java
@@ -1,7 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
+/update
 package GUI;
 
 import javax.swing.*;
